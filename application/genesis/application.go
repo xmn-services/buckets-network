a222e041f8dbbb47c2f71969bf76c84bf66c27f8
Steve Rodrigue <stev.rodr@gmail.com>
2020-11-11 04:14:13 -0500

    Fixed the genesis application by remocing the current app

--- application/genesis/application.go
+++ application/genesis/application.go
@@ -1,20 +1,54 @@
 package genesis
 
+import (
+	"errors"
+	"time"
+
+	"github.com/xmn-services/buckets-network/domain/memory/genesis"
+)
+
 type application struct {
-	current Current
+	genesisBuilder    genesis.Builder
+	genesisRepository genesis.Repository
+	genesisService    genesis.Service
 }
 
 func createApplication(
-	current Current,
+	genesisBuilder genesis.Builder,
+	genesisRepository genesis.Repository,
+	genesisService genesis.Service,
 ) Application {
 	out := application{
-		current: current,
+		genesisBuilder:    genesisBuilder,
+		genesisRepository: genesisRepository,
+		genesisService:    genesisService,
 	}
 
 	return &out
 }
 
-// Current returns the current application
-func (app *application) Current() Current {
-	return app.current
+// Init initializes the genesis block
+func (app *application) Init(
+	blockDifficultyBase uint,
+	blockDifficultyIncreasePerTrx float64,
+	linkDifficulty uint,
+) error {
+	_, err := app.genesisRepository.Retrieve()
+	if err == nil {
+		return errors.New("the genesis block has already been created")
+	}
+
+	createdOn := time.Now().UTC()
+	gen, err := app.genesisBuilder.Create().
+		WithBlockDifficultyBase(blockDifficultyBase).
+		WithBlockDifficultyIncreasePerTrx(blockDifficultyIncreasePerTrx).
+		WithLinkDifficulty(linkDifficulty).
+		CreatedOn(createdOn).
+		Now()
+
+	if err != nil {
+		return err
+	}
+
+	return app.genesisService.Save(gen)
 }
--- application/genesis/sdk.go
+++ application/genesis/sdk.go
@@ -2,11 +2,6 @@ package genesis
 
 // Application represents the genesis application
 type Application interface {
-	Current() Current
-}
-
-// Current represents the current application
-type Current interface {
 	Init(
 		blockDifficultyBase uint,
 		blockDifficultyIncreasePerTrx float64,
