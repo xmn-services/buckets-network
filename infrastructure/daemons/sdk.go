c59c370032c8711637a07b973a7d648ae0164faf
Steve Rodrigue <stev.rodr@gmail.com>
2020-11-16 03:09:31 -0500

    Removed unnecessary code

--- infrastructure/daemons/sdk.go
+++ infrastructure/daemons/sdk.go
@@ -1,10 +1,1 @@
 package daemons
-
-// maxMiningValue represents the max mining value before adding another miner number to the slice
-const maxMiningValue = 2147483647
-
-// maxMiningTries represents the max mining characters to try before abandonning
-const maxMiningTries = 2147483647
-
-// miningBeginValue represents the first value of the hash that is expected on mining
-const miningBeginValue = 0
